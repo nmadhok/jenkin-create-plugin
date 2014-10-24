6867f7c8e9c840d6175b058b39a61b80d0723547
root <root@nitin-jenkins-training.(none)>
2014-10-23 20:24:38 -0400

    Adding BeerBuilder

--- src/main/java/org/jenkinsci/plugins/awesome/BeerBuilder.java
+++ src/main/java/org/jenkinsci/plugins/awesome/BeerBuilder.java
@@ -1,8 +1,11 @@
 package prg.jenkinsci.plugins.awesome;
 
+import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
+import hudson.model.FreeStyleProject;
+import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -18,6 +21,19 @@ public class BeerBuilder extends Builder {
 			BuildListener listener) {
     PrintStream console = listener.getLogger();
     console.println("hello JUC");
-    
+    return true;    
   }
+
+  @Extension
+  public static class DescriptorImpl extends BuildStepDescriptor<Builder> {
+    @Override
+    public boolean isApplicable(Class aClass) {
+      return true;
+    }
+
+    @Override
+      public String getDisplayName() {
+        return "Beer builder";
+      }
+    }
 }
